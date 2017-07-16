516863074c86d67487e9bd3bfd2e0c30312f11f4
KiselevNikolayGit <neondoctors@gmail.com>
2017-07-16 11:14:11 +0300

    pre-sedlypen

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var elems = document.getElementsByClassName("prapp");
 
 window.addEventListener('scroll', function() {
 	// Чет не гладенько, даже хз
-	var pos = window.pageYOffset * 0.007;
+	var pos = window.pageYOffset * 0.02;
 	for (i = 0; i < elems.length; i++) {
 		elems.item(i).style.backgroundPosition = "0% ".concat((50 + pos).toString()).concat("%");
 	};
