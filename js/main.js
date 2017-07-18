995ebb31ccc9db1bf6bdfca89cce28917cb038b4
KiselevNikolayGit <neondoctors@gmail.com>
2017-07-18 13:10:10 +0300

    final design version

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var elems = document.getElementsByClassName("prapp");
 
 window.addEventListener('scroll', function() {
-	// Чет не гладенько, даже хз
-	var pos = window.pageYOffset * 0.02;
+	var pos = window.pageYOffset * 0.04;
 	for (i = 0; i < elems.length; i++) {
-		elems.item(i).style.backgroundPosition = "0% ".concat((50 + pos).toString()).concat("%");
+		elems.item(i).style.backgroundPosition = "0% ".concat((pos).toString()).concat("%");
 	};
 });
