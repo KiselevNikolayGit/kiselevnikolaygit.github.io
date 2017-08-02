112e4c0112b0bd4a40d01ce6b1e3b9f20ad3c82b
KiselevNikolayGit <neondoctors@gmail.com>
2017-08-02 18:05:28 +0300

    Massive update. SO MASSIVE!

--- js/home10.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function () {
-   window.location.href = "index.html";
-}, 4000); //will call the function after 2 secs.
--- js/main.js
+++
@@ -1,8 +0,0 @@
-var elems = document.getElementsByClassName("prapp");
-
-window.addEventListener('scroll', function() {
-	var pos = window.pageYOffset * 0.04;
-	for (i = 0; i < elems.length; i++) {
-		elems.item(i).style.backgroundPosition = "0% ".concat((pos).toString()).concat("%");
-	};
-});
