e78f2ba21b90c920dd05f6eacd5b2f1b83ec3014
Nikolay <kn@localhost.localdomain>
2017-09-01 21:56:14 +0300

    1.9

---
+++ js/para.js
@@ -0,0 +1,15 @@
+// var list = document.getElementsByClassName("pra");
+
+// window.onscroll = function() {
+//     for (var item of list) {
+//         var bodyRect = document.body.getBoundingClientRect();
+//         var elemRect = item.getBoundingClientRect();
+//         var offset = elemRect.top - bodyRect.top;
+//         offset = offset / 500;
+//         offset = offset.toString() + "px";
+//         var ss = "0px ";
+//         ss = ss + offset;
+//         item.style["background-position"] = ss;
+//         alert('Element is ' + offset + ' vertical pixels from <body>');
+//     };
+// };
