2917270176beed6c024f21b8856a41269c3d1c1d
Nikolay <kn@localhost.localdomain>
2017-09-16 09:03:52 +0300

    MEGGA JS LOAD

--- js/aftersmooth.js
+++ js/aftersmooth.js
@@ -1,4 +1,11 @@
+setTimeout(function() { window.scroll(0, 0); }, 130);
+
 function nowhl(){
     return false;
 };
 window.onwheel = nowhl;
+
+function onlo() {
+    document.getElementById("spinner").style.animation = "nospin 1s cubic-bezier(0.165, 0.84, 0.44, 1) forwards 1";
+};
+window.onload = function() {setTimeout(onlo, 1000);};
