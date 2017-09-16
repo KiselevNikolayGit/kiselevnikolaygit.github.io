029613030859808db9cec265c0355170b0ec7bc7
Nikolay <kn@localhost.localdomain>
2017-09-16 19:58:11 +0300

    cross-scroll

--- js/aftersmooth.js
+++ js/aftersmooth.js
@@ -1,9 +1,10 @@
-setTimeout(function() { window.scroll(0, 0); }, 130);
-
-function nowhl(){
-    return false;
+function home() {
+    window.scroll(0, 0);
 };
-window.onwheel = nowhl;
+
+setTimeout(home, 130);
+
+window.onwheel = function() {return false};
 
 function onlo() {
     document.getElementById("spinner").style.animation = "nospin 1s cubic-bezier(0.165, 0.84, 0.44, 1) forwards 1";
--- js/smooth.js
+++ js/smooth.js
@@ -1,17 +1,18 @@
-var vw = document.getElementById("hdr").offsetHeight
+console.log("Kiselev Nikolay is good.");    
+var vh = document.getElementById("hdr").offsetHeight;
 
 function scrlup() {
-    window.scrollBy({
-        top: -vw,
-        left: 0, 
-        behavior: 'smooth' 
-    });
+    for (var index = 0; index < vh; index++) {
+        setTimeout(function() {
+            document.body.scrollBy(0, -1);
+        }, (index * .6 + (index * 0.002)));
+    };
 };
 
 function scrldown() {
-    window.scrollBy({
-        top: vw,
-        left: 0, 
-        behavior: 'smooth' 
-    });
+    for (var index = 0; index < vh; index++) {
+        setTimeout(function() {
+            document.body.scrollBy(0, 1);
+        }, (index * .6 + (index * 0.002)));
+    };  
 };
