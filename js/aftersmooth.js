02bffc09adb998fd866626116c8bd8ad95062c43
Nikolay <kn@localhost.localdomain>
2017-09-17 13:36:54 +0300

    moon shine

--- js/aftersmooth.js
+++ js/aftersmooth.js
@@ -8,5 +8,6 @@ window.onwheel = function() {return false};
 
 function onlo() {
     document.getElementById("spinner").style.animation = "nospin 1s cubic-bezier(0.165, 0.84, 0.44, 1) forwards 1";
+    alert("Колян тут шаманит! Тут ремонт, не смотри пожалуйста.");
 };
-window.onload = function() {setTimeout(onlo, 1000);};
+window.onload = function() {setTimeout(onlo, 50);};
--- js/smooth.js
+++ js/smooth.js
@@ -16,3 +16,11 @@ function scrldown() {
         }, (index * .6 + (index * 0.002)));
     };  
 };
+
+function scrltonav() {
+    for (var index = 0; index < vh; index++) {
+        setTimeout(function() {
+            document.body.scrollBy(0, -3);
+        }, (index * .6 + (index * 0.002)));
+    };  
+};
