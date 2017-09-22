0ecb9ac8a7ba16000622343c03d20db3922c74fb
Nikolay <kn@localhost.localdomain>
2017-09-22 12:38:49 +0300

    jquery!!!!!!

--- js/aftersmooth.js
+++ js/aftersmooth.js
@@ -1,13 +1,67 @@
+// Kisel made dat shit.
+
+
 function home() {
     window.scroll(0, 0);
 };
-
 setTimeout(home, 130);
-
-window.onwheel = function() {return false};
 
 function onlo() {
     document.getElementById("spinner").style.animation = "nospin 1s cubic-bezier(0.165, 0.84, 0.44, 1) forwards 1";
-    alert("Колян тут шаманит! Тут ремонт, не смотри пожалуйста.");
+    alert("Николай Киселев работает над тем, чтоб хоть что-то перестало не работать и отвелекать от работы.");
 };
 window.onload = function() {setTimeout(onlo, 50);};
+
+
+// МНЕ ОЧЕНЬ ЖАЛЬ ЧТО ПРИШЛОСЬ ПИСАТЬ
+// СКРИПТ НА ПИТОНЕ ДЛЯ СЛЕДУЮЩИХ СТРОК.
+// ЧТО-ТО ЦИКЛЫ В JS НЕ ПОСЛУШНЫЕ СОВСЕМ.
+
+
+$("#d1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#it2").offset().top
+    }, 1000);
+});
+
+$("#d2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#it3").offset().top
+    }, 1000);
+});
+
+$("#d3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#it4").offset().top
+    }, 1000);
+});
+
+$("#d4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#it5").offset().top
+    }, 1000);
+});
+
+$("#u2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#it1").offset().top
+    }, 1000);
+});
+
+$("#u3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#it2").offset().top
+    }, 1000);
+});
+
+$("#u4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#it3").offset().top
+    }, 1000);
+});
+
+$("#u5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#it1").offset().top
+    }, 1000);
+});
