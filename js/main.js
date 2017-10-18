aa226b64eff590f4b1f9f179d16f981fbea9c4b3
Nikolay <artelbear@gmail.com>
2017-10-18 21:36:49 +0300

    New episode

--- js/main.js
+++ js/main.js
@@ -1,26 +1,13 @@
+alert("SITE IS NOT SUCH NOT YOU KNOW WHAT I MEEN?");
+
 var windowWidth = window.screen.width < window.outerWidth ?
 window.screen.width : window.outerWidth;
 var mobile = windowWidth < 500;
 
+$("#main-header").css("height", $("#logo").css("height"));
+
 function show_nav() {
-    if ($("#nav-in").css("display") != "none") {
-        $("#nav-in").css("display", "none");
-        $("#nav-out").css("width", "auto");
-        $("#nav-out").css("height", "auto");
-        if (mobile) {
-            $("#nav-field").show();
-        };
-    } else {
-        l = $("#nav-field").height();
-        $("#nav-in").css("display", "flex");
-        if (mobile) {
-            $("#nav-out").css("height", l + "px");
-            $("#nav-out").css("width", windowWidth + "px");
-            $("#nav-field").hide();
-        } else {
-            $("#nav-out").css("width", "50%");
-        };
-    };
+
 };
 
 show_nav();
