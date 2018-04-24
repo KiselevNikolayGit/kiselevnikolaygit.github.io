5111baed281f84f4c79ed43287bc99acb25046f8
nikolay <artelbear@gmail.com>
2018-04-25 00:02:44 +0300

    test handredirecter

--- js/wholesystem.vue.js
+++ js/wholesystem.vue.js
@@ -1,4 +1,8 @@
-let open_document = "sus";
+let rule = localStorage.getItem('docu');
+rule = rule.replace("http://yellow-hi.tech/", "");
+localStorage.setItem('docu', "");
+
+let open_document = rule;
 
 var app = new Vue({
   el: '#wholesystem',
