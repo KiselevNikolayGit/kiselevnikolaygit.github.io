ebe2536fb9c8fcbda308ab7614bfb530b3c59a3f
nikolay <artelbear@gmail.com>
2018-04-25 00:22:09 +0300

    test publish

--- js/wholesystem.vue.js
+++ js/wholesystem.vue.js
@@ -1,13 +1,44 @@
-let rule = localStorage.getItem('docu');
-rule = rule.replace("http://yellow-hi.tech/", "");
-localStorage.setItem('docu', "");
+var quill = new Quill('#editor', {
+	modules: {
+		toolbar: [
+			[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+			['bold', 'italic', 'underline', 'strike'],
+			[{ 'font': [] }],
+			[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+			[{ 'indent': '-1'}, { 'indent': '+1' }],
+			// [{ 'color': [] }, { 'background': [] }],
+			[{ 'align': [] }],
+			['blockquote', 'code-block']
+		]
+	},
+	placeholder: 'Мыслям загорелся зеленый цвет...',
+	theme: 'snow'
+});
 
-let open_document = rule;
+let open_document = localStorage.getItem('docu');
+open_document = open_document.replace("http://yellow-hi.tech/", "");
 
 var app = new Vue({
-  el: '#wholesystem',
-  data: {
-    adress: open_document,
-    adressold: open_document
-  }
+	el: '#wholesystem',
+	data: {
+    	adress: open_document,
+	    adressold: open_document
+	}
 });
+
+function sync() {
+	localStorage.setItem('docu', app.adress);
+	app.adressold = app.adress;
+	open_document = app.adress;
+	let docRef = db.collection("notes").doc(open_document);
+	docRef.get().then(function(doc) {
+	    if (doc.exists) {
+	        let data = doc.data();
+        	quill.setContents(data),
+	    } else {
+	        docRef.set({
+				body: quill.getContents(),
+			});
+	    }
+	});
+}
